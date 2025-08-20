70c5ae0611400500c98b85ee67e7a9ae56809170
ayaa-oss <cisiliaaura570@gmail.com>
2025-08-20 13:16:37 +0700

    day 1 upload portofolio

---
+++ assets/images/project/sketch.ino
@@ -0,0 +1,27 @@
+// Program : led Blink
+// Program : Cisilia Aura
+// Version : 0.1
+// Date : 27-7-2025
+
+void setup() {
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  delay(1000);
+
+}
